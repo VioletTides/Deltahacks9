5b1dc3659504ade010fcd235903f0f486ae37883
trinity-y <iblooxd@gmail.com>
2023-01-15 03:52:57 -0500

    example lat long on exports page

--- src/pages/Exports.js
+++ src/pages/Exports.js
@@ -1,35 +1,39 @@
 import { auth, db } from "../firebase-config";
-import { set, ref } from "firebase/database";
+import { set, get, ref } from "firebase/database";
+import React, { useRef, useEffect, useState } from "react"
 
 
 // This is the home page of the website, write all the code for the home page here
 export default function Exports(){
-    // const latRef = useRef();
-    // const longRef = useRef();
+    const latRef = useRef();
+    const longRef = useRef();
+    
+    const [lat, setLat] = useState(null);
+    const [long, setLong] = useState(null);
 
-    // async function getLatLong(uid) {
-    //     let lat, long;
-    //     await get(ref(db, `${uid}`))
-    //         .then(snapshot => {
-    //             const userData = snapshot.val();
-    //             lat = userData.lat;
-    //             long = userData.long;
-    //         })
-    //         .catch(error => {
-    //             console.log(error);
-    //         });
-    //     return { lat, long };
-    // }
-
-    // useEffect(() => {
-    //     if (currentUser) {
-    //         getLatLong(currentUser.uid)
-    //             .then(({ lat, long }) => {
-    //                 latRef.current.value = lat;
-    //                 longRef.current.value = long;
-    //             });
-    //     }
-    // }, [currentUser]);
+    async function getLatLong() {
+        let lat, long;
+        await get(ref(db, `${auth.currentUser.uid}`))
+            .then(snapshot => {
+                const userData = snapshot.val();
+                lat = userData.lat;
+                long = userData.long;
+            })
+            .catch(error => {
+                console.log(error);
+            });
+        return { lat, long };
+    }
+    
+    useEffect(() => {
+        if (auth.currentUser) {
+            getLatLong(auth.currentUser.uid)
+                .then(({ lat: latValue, long: longValue }) => {
+                    setLat(latValue);
+                    setLong(longValue);
+                });
+        }
+    }, [auth.currentUser])
 
     return(
         <div>
@@ -41,7 +45,7 @@ export default function Exports(){
                 </div>
                 <div className="col">
                     <div className = "row">
-
+                        <p>heyyy y peopleeeee your lat is {lat} and your long is {long} lollldllfoslkfjdskj</p>
                     </div>
 
                     <div className = "row">
