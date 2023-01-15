d795322306467443d3a2668d4999da4b81a0c432
Ross Anderson <ross34andeson@gmail.com>
2023-01-14 22:31:07 -0500

    dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,38 @@
+import React, { useState } from 'react';
+import { Card, Button, Alert } from 'react-bootstrap';
+import { useAuth } from './AuthContext';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Dashboard() {
+    const [error, setError] = useState("")
+    const { currentUser, logout } = useAuth()
+    const Navigate = useNavigate()
+
+    function handleLogout() {
+        setError("")
+
+        try {
+            await logout()
+            Navigate("/login")
+        } catch(error) {
+            setError("Failed to log out")
+        }
+    }
+
     return(
-        <h2>Dashboard</h2>
+        <>
+            <Card>
+                <Card.Body>
+                    <h2 className="text-center mb-4">Profile</h2>
+                    {error && <Alert variant="danger">{error}</Alert>}
+                    <strong>Email:</strong> {currentUser.email}
+                    <Link to="/update-profile" className="btn btn-primary w-100 mt-3">Update Profile</Link>
+                </Card.Body>
+
+            </Card>
+            <div className="w-100 text-center mt-2">
+                <Button variant="link" onClick={handleLogout}>Log Out</Button>
+            </div>
+        </>
     ) 
 }
