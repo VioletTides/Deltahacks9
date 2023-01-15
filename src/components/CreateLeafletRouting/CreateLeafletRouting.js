bdc0802c1c1f97ce013ceb5150827dc93c106714
Winswins22 <73000223+Winswins22@users.noreply.github.com>
2023-01-15 03:16:54 -0500

    remove interneary on leaflet map

--- src/components/CreateLeafletRouting/CreateLeafletRouting.js
+++ src/components/CreateLeafletRouting/CreateLeafletRouting.js
@@ -1,20 +1,7 @@
 // TODO: add latlong firebase requestion function and leaflet route creation function, add pointers for all firebase data, getDistance()
-import { useState } from "react"
-import createRoutineMachineLayer from "../LeafletMap/CreateLeafletRoute"
+import { useState, useContext } from "react"
 
 const CreateLeafletRouting = () => {
-
-    let x1 = 43.777702
-    let y1 = -79.233238
-    let x2 = 44.777702
-    let y2 = -79.233238
-
-    const [coords, setCoords] = useState([[x1, y1],[x2, y2]])
-
-    return 
-    <>
-
-    </>
 }
 
 export default CreateLeafletRouting
--- src/components/LeafletMap/LeafletMap.js
+++ src/components/LeafletMap/LeafletMap.js
@@ -1,17 +1,14 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import LeafletRouting from "./CreateLeafletRoute"
-
-// import "leaflet/dist/leaflet.css";
+import {CreateLeafletRoute, CreateLeafletRoute2} from './CreateLeafletRoute';
+import "./leaflet_support.css"
 
 const LeafletMap = () => {
-
     const defaultTestPosition = [43.777702, -79.233238];
-    
+
     return (
         <>
-            {/* The Leaflet map. All components except LeafletRouting are default React Leaflet */}
-            <MapContainer center={defaultTestPosition} zoom={8} scrollWheelZoom={false} style={{width: '500px', height: '500px'}}>
+            <MapContainer center={defaultTestPosition} zoom={8} scrollWheelZoom={false} style={{width: '1000px', height: '1000px'}}>
                 <TileLayer
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -22,11 +19,12 @@ const LeafletMap = () => {
                 </Popup>
                 </Marker>
 
-                <LeafletRouting></LeafletRouting>
-
+                <CreateLeafletRoute></CreateLeafletRoute>
             </MapContainer>
         </>
     )
 }
+
+
 
 export default LeafletMap
